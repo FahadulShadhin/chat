b2c53db877694d12b3d1ff601b5693cb3abd5652
FahadulShadhin <fahadulshadhin17@gmail.com>
2023-04-13 09:47:06 +0600

    signup

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -1,19 +1,137 @@
 import React, { useState } from 'react';
-import { FormControl, FormLabel, VStack, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import {
+	FormControl,
+	FormLabel,
+	VStack,
+	Button,
+	useToast,
+} from '@chakra-ui/react';
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/input';
 
 const Signup = () => {
-	const [show, setShow] = useState(false);
+	const [showPass, setShowPass] = useState(false);
+	const [showConfirmPass, setShowConfirmPass] = useState(false);
 	const [name, setName] = useState();
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
 	const [confirmPassword, setConfirmPassword] = useState();
+	const [picture, setPicture] = useState();
+	const [loading, setLoading] = useState(false);
+	const toast = useToast();
+	const navigate = useNavigate();
 
-	const handleClick = () => setShow(!show);
+	const handlePassShowClick = () => setShowPass(!showPass);
+	const handleConfirmPassShowClick = () => setShowConfirmPass(!showConfirmPass);
 
-	const handleSubmit = () => {};
+	const handleSubmit = async () => {
+		setLoading(true);
 
-	const postDetails = (pictures) => {};
+		if (!name || !email || !password || !confirmPassword) {
+			toast({
+				title: 'Please fill up the fields',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+			setLoading(false);
+			return;
+		}
+
+		if (password !== confirmPassword) {
+			toast({
+				title: 'Passwords do not match',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+		}
+
+		try {
+			const config = {
+				headers: {
+					'Content-type': 'application/json',
+				},
+			};
+
+			const { data } = await axios.post(
+				'/api/user',
+				{ name, email, password, picture },
+				config
+			);
+			toast({
+				title: 'Registration successfull',
+				status: 'success',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+
+			setLoading(false);
+			navigate('/');
+		} catch (error) {
+			toast({
+				title: 'Error occured!',
+				description: error.response.data.message,
+				status: 'error',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+			setLoading(false);
+		}
+	};
+
+	const postDetails = async (pictures) => {
+		setLoading(true);
+		if (pictures === undefined) {
+			toast({
+				title: 'Please select an image.',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+			return;
+		}
+
+		if (
+			pictures.type === 'image/jpeg' ||
+			pictures.type === 'image/jpg' ||
+			pictures.type === 'image/png'
+		) {
+			const data = new FormData();
+
+			data.append('file', pictures);
+			data.append('upload_preset', 'talkit');
+			data.append('cloud_name', 'dv7lwdftm');
+
+			try {
+				const res = await axios.post(
+					'https://api.cloudinary.com/v1_1/dv7lwdftm/image/upload',
+					data
+				);
+				setPicture(res.data.url.toString());
+				setLoading(false);
+			} catch (err) {
+				console.log(err);
+				setLoading(false);
+			}
+		} else {
+			toast({
+				title: 'Please select an image.',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+				position: 'bottom',
+			});
+			setLoading(false);
+			return;
+		}
+	};
 
 	return (
 		<VStack spacing="5px">
@@ -37,13 +155,13 @@ const Signup = () => {
 				<FormLabel>Password</FormLabel>
 				<InputGroup>
 					<Input
-						type={show ? 'text' : 'password'}
+						type={showPass ? 'text' : 'password'}
 						placeholder="********"
 						onChange={(event) => setPassword(event.target.value)}
 					/>
 					<InputRightElement width="4.5rem">
-						<Button h="1.75rem" size="sm" onClick={handleClick}>
-							{show ? 'Hide' : 'Show'}
+						<Button h="1.75rem" size="sm" onClick={handlePassShowClick}>
+							{showPass ? 'Hide' : 'Show'}
 						</Button>
 					</InputRightElement>
 				</InputGroup>
@@ -53,13 +171,13 @@ const Signup = () => {
 				<FormLabel>Confirm Password</FormLabel>
 				<InputGroup>
 					<Input
-						type={show ? 'text' : 'password'}
+						type={showConfirmPass ? 'text' : 'password'}
 						placeholder="********"
 						onChange={(event) => setConfirmPassword(event.target.value)}
 					/>
 					<InputRightElement width="4.5rem">
-						<Button h="1.75rem" size="sm" onClick={handleClick}>
-							{show ? 'Hide' : 'Show'}
+						<Button h="1.75rem" size="sm" onClick={handleConfirmPassShowClick}>
+							{showConfirmPass ? 'Hide' : 'Show'}
 						</Button>
 					</InputRightElement>
 				</InputGroup>
@@ -80,6 +198,7 @@ const Signup = () => {
 				width="100%"
 				style={{ marginTop: 15 }}
 				onClick={handleSubmit}
+				isLoading={loading}
 			>
 				Sign up
 			</Button>
